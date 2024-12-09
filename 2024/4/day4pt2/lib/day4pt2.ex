eaf11ee8ddc66393d6ad8ef49c23e87349f7bd02
shwoop <monkey.mambo.al.ferguson@gmail.com>
2024-12-09 22:06:52 +0100

    playing with mix

---
+++ 2024/4/day4pt2/lib/day4pt2.ex
@@ -0,0 +1,22 @@
+defmodule Day4pt2 do
+  @moduledoc """
+  Documentation for `Day4pt2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day4pt2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def main(_args) do
+    IO.puts "poop"
+  end
+end
