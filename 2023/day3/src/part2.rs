485b9e4066e2f46588557a0e6982158c01d603f4
shwoop <monkey.mambo.al.ferguson@gmail.com>
2025-12-16 21:02:02 +0100

    wip

--- 2023/day3/src/part2.rs
+++ 2023/day3/src/part2.rs
@@ -1,42 +1,86 @@
 use std::{collections::HashMap, io::BufRead};
 //use tracing::debug;
 
-use crate::parts::{ParsedRow,Part,PartNumber,parse_line};
+use crate::parts::{ParsedRow, Part, PartNumber, parse_line};
 
+struct FirstNumber {
+    pub id: usize,
+    pub from: usize,
+    pub to: usize,
+}
+
+struct Cog {
+    location: usize,
+}
+
+struct NumberWithCog {
+    first_number_id:  usize,
+    cog_location: usize,
+}
+
+impl FirstNumber {
+    fn touches(&self, cog_location: usize) -> Option<NumberWithCog> {
+        let mut from = self.from;
+        if from != 0 {
+            from -= 1;
+        }
+
+        if cog_location >= from && cog_location <= self.to {
+            return Some(NumberWithCog {
+                first_number_id: self.id,
+                cog_location: cog_location
+            });
+        }
+
+        None
+    }
+}
+
+impl NumberWithCog {
+    fn touches(&self, pn: &PartNumber) -> usize {
+        let mut from = pn.from;
+        if from != 0 {
+            from -= 1;
+        }
+
+        if self.cog_location >= from && self.cog_location <= pn.to {
+            return self.first_number_id * pn.id;
+        }
+
+        0
+    }
+}
 
 pub fn part2<R: BufRead>(reader: R) -> Result<usize, Box<dyn std::error::Error>> {
     let mut acc = 0;
-    let mut previous_row: Option<ParsedRow> = None;
-
-    let mut cogs: Vec<(usize, usize)> = Vec::new();
-
-    let mut lines: HashMap<usize, String> = HashMap::new();
+    let mut previous_first_numbers: Vec<FirstNumber> = Vec::new();
+    let mut previous_number_with_cogs: Vec<NumberWithCog> = Vec::new();
 
     for (y, line) in reader.lines().enumerate() {
         let line = line.unwrap();
-        lines.insert(y, line.clone());
+        let (part_numbers, parts) = parse_line(line.as_str());
 
-        for (x, char) in line.char_indices() {
-            if char == '*' {
-                cogs.push((x, y))
+        let cogs: Vec<&Part> = parts.iter().filter(|p| -> bool { p.icon == '*' }).collect();
+
+        for previous_number_with_cog in previous_number_with_cogs.iter() {
+            for part_number in part_numbers.iter() {
+                acc += previous_number_with_cog.touches(part_number);
+            }
+        }
+
+        previous_number_with_cogs = Vec::new();
+        for previous_first_number in previous_first_numbers.iter() {
+            for cog in cogs.iter() {
+                match previous_first_number.touches(cog.location) {
+                    Some(pnwc)=> previous_number_with_cogs.push(pnwc),
+                    None => (),
+                }
             }
         }
     }
-    println!("{:?}", cogs);
-    println!("{:?}", lines);
 
     Ok(acc)
 }
-
-pub fn check_cogs(
-    line_length: usize,
-    previous_row: Option<ParsedRow>,
-    part_numbers: &Vec<PartNumber>,
-    parts: &Vec<Part>,
-) -> usize {
-    0
-}
-
 
 #[cfg(test)]
 mod test_part_2 {
