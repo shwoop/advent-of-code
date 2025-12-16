ff3803f60dce1b291e45a7d1ae6c77f627607de2
shwoop <monkey.mambo.al.ferguson@gmail.com>
2025-12-16 13:08:10 +0100

    same line logic working

--- 2023/day3/src/main.rs
+++ 2023/day3/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+
+mod part1;
+// mod part2;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+
+
+    if let Ok(s) = env::var("PART") && s == "2" {
+        //let result = part2::part2(reader)?;
+        let result = 0;
+        println!("result 2 is {}", result);
+    } else {
+        let result = part1::part1(reader)?;
+        println!("result 1 is {}", result);
+    }
+
+    Ok(())
 }
---
+++ 2023/day3/src/part1.rs
@@ -0,0 +1,136 @@
+use regex::Regex;
+use std::io::BufRead;
+
+pub fn part1<R: BufRead>(reader: R) -> Result<usize, Box<dyn std::error::Error>> {
+    let mut acc = 0;
+    let mut previous_row: Option<ParsedRow> = None;
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let (mut part_numbers, parts) = parse_line(line.as_str());
+
+        println!("{:?} {:?}", part_numbers, parts);
+        acc += check_collisions(line.len(), previous_row, &mut part_numbers, &parts);
+
+        previous_row = Some((part_numbers, parts));
+    }
+
+    Ok(acc)
+}
+
+#[derive(Debug)]
+struct PartNumber {
+    id: usize,
+    from: usize,
+    to: usize,
+}
+
+#[derive(Debug)]
+struct Part {
+    location: usize,
+}
+
+type ParsedRow = (Vec<PartNumber>, Vec<Part>);
+
+fn check_collisions(
+    line_length: usize,
+    previous_row: Option<ParsedRow>,
+    part_numbers: &mut Vec<PartNumber>,
+    parts: &Vec<Part>,
+) -> usize {
+    let mut acc: usize = 0;
+    let mut parts_to_remove: Vec<usize> = Vec::new();
+
+    // check current row
+    for (i, part_number) in part_numbers.iter().enumerate() {
+        for part in parts.iter() {
+            if (part_number.from != 0 && part_number.from - 1 == part.location)
+                || (part_number.to != line_length && part_number.to == part.location)
+            {
+                // matched
+                acc += part_number.id;
+                parts_to_remove.push(i);
+            }
+        }
+    }
+
+    for i in parts_to_remove {
+        part_numbers.remove(i);
+    }
+
+    // check previous row
+    // todo
+
+    acc
+}
+
+fn parse_line(line: &str) -> ParsedRow {
+    let mut part_numbers: Vec<PartNumber> = Vec::new();
+    let mut parts: Vec<Part> = Vec::new();
+
+    let re = Regex::new(r"\d+").unwrap();
+    for mat in re.find_iter(&line) {
+        part_numbers.push(PartNumber {
+            id: mat.as_str().parse::<usize>().unwrap(),
+            from: mat.start(),
+            to: mat.end(),
+        });
+    }
+    for (i, char) in line.char_indices() {
+        if char == '.' || char.is_numeric() {
+            continue;
+        }
+        parts.push(Part { location: i })
+    }
+
+    (part_numbers, parts)
+}
+
+#[cfg(test)]
+mod test_part_1 {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn example() {
+        let input = r#"467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."#;
+        let expected = 4361;
+
+        let cur = Cursor::new(input.as_bytes());
+        let result = part1(cur).unwrap();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn partial_1() {
+        let input = r#"467..114..
+...*......"#;
+        let expected = 467;
+
+        let cur = Cursor::new(input.as_bytes());
+        let result = part1(cur).unwrap();
+
+        assert_eq!(result, expected);
+    }
+
+        #[test]
+    fn partial_2() {
+        let input = r"617*......";
+        let expected = 617;
+
+        let cur = Cursor::new(input.as_bytes());
+        let result = part1(cur).unwrap();
+
+        assert_eq!(result, expected);
+    }
+}
