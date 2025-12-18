29ecdcb25191513c8a20ba456bee02c85276ff20
shwoop <monkey.mambo.al.ferguson@gmail.com>
2025-12-18 09:06:59 +0100

    extract info

--- 2025/10/internal/part1.go
+++ 2025/10/internal/part1.go
@@ -16,11 +16,13 @@ type Doc struct {
 	indicatorLights    int
 	lenIndicatorLights int
 	buttons            []int
+	joltages           []int
+	buttonsArrays      [][]int
 }
 
 func (d Doc) String() string {
 	binaryPattern := fmt.Sprintf("%%0%db", d.lenIndicatorLights)
-	buttonPattern := "(" + binaryPattern + ")"
+	buttonPattern := "(" + binaryPattern + ", %v)"
 
 	b := strings.Builder{}
 
@@ -31,10 +33,16 @@ func (d Doc) String() string {
 
 	buttons := make([]string, len(d.buttons))
 	for i, button := range d.buttons {
-		buttons[i] = fmt.Sprintf(buttonPattern, button)
+		buttons[i] = fmt.Sprintf(buttonPattern, button, d.buttonsArrays[i])
 	}
 
 	b.WriteString(strings.Join(buttons, ", "))
+
+	joltages := []string{}
+	for _, j := range d.joltages {
+		joltages = append(joltages, fmt.Sprintf("%d", j))
+	}
+	b.WriteString(fmt.Sprintf(" {%s}", strings.Join(joltages, ",")))
 
 	return b.String()
 }
@@ -71,17 +79,30 @@ func ParseDoc(line string) Doc {
 	indicatorString = strings.ReplaceAll(indicatorString, "#", "1")
 	indicatorLights, _ := strconv.ParseInt(indicatorString, 2, 32)
 
-	doc := Doc{int(indicatorLights), len(indicatorString), []int{}}
+	doc := Doc{int(indicatorLights), len(indicatorString), []int{}, []int{}, [][]int{}}
 
 	buttons := buttonRegexp.FindAllString(line, -1)
 	for _, buttonString := range buttons {
 		var button int
+		buttonArray := make([]int, doc.lenIndicatorLights)
+
 		for num := range strings.SplitSeq(buttonString[1:len(buttonString)-1], ",") {
 			n, _ := strconv.Atoi(num)
 			shift := (doc.lenIndicatorLights - 1 - n)
 			button |= 1 << shift
+
+			buttonArray[n] = 1
 		}
+
 		doc.buttons = append(doc.buttons, button)
+
+		doc.buttonsArrays = append(doc.buttonsArrays, buttonArray)
+	}
+
+	joltageStrings := strings.Split(line[strings.Index(line, "{")+1:len(line)-1], ",")
+	for _, js := range joltageStrings {
+		n, _ := strconv.Atoi(js)
+		doc.joltages = append(doc.joltages, n)
 	}
 
 	return doc
