7a7ad187f35d15934d99b6c0dadb58d6ab2f5929
shwoop <monkey.mambo.al.ferguson@gmail.com>
2025-12-18 12:15:47 +0100

    cache masks that we can apply

--- 2025/10/internal/part2.go
+++ 2025/10/internal/part2.go
@@ -2,8 +2,11 @@ package internal
 
 import (
 	"bufio"
+	"cmp"
 	"fmt"
 	"math"
+	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -45,19 +48,75 @@ func printHistory(w *strings.Builder, h *History) *strings.Builder {
 type Accumulator struct {
 	masks         [][]int
 	shortestDepth int
+	maskMap       map[string][][]int
 
 	history *History
 }
 
 func (d Doc) SolvePart2() int {
+	slices.SortFunc(d.buttonsArrays, func(a, b []int) int {
+		var powA, powB int
+		var aBinaryString, bBinaryString string
+
+		for i := range d.lenIndicatorLights {
+			powA += a[i]
+			powB += b[i]
+
+			aBinaryString += fmt.Sprintf("%d", a[i])
+			bBinaryString += fmt.Sprintf("%d", b[i])
+		}
+
+		// primarily order by number of bits flipped
+		if powA != powB {
+			return cmp.Compare(powA, powB) * -1
+		}
+
+		// secondary, order by highest bit
+		aInt, _ := strconv.ParseInt(aBinaryString, 2, 64)
+		bInt, _ := strconv.ParseInt(bBinaryString, 2, 64)
+
+		return cmp.Compare(aInt, bInt) * -1
+	})
+
+	maskMap := map[string][][]int{}
+	for _, mask := range d.buttonsArrays {
+		maskStrings := AllPossibleBitPatterns(mask)
+		for _, ms := range maskStrings {
+			maskMap[ms] = append(maskMap[ms], mask)
+		}
+	}
+
 	acc := Accumulator{
 		masks:         d.buttonsArrays,
 		shortestDepth: math.MaxInt,
+		maskMap:       maskMap,
 	}
 
 	solvePart2(0, d.joltages, make([]int, d.lenIndicatorLights), &acc)
 
 	return acc.shortestDepth
+}
+
+func AllPossibleBitPatterns(ba []int) []string {
+	result := []string{}
+
+	r := []string{"X"}
+	if ba[0] == 0 {
+		r = append(r, "0")
+	}
+
+	if len(ba) == 1 {
+		return r
+	}
+
+	app := AllPossibleBitPatterns(ba[1:])
+	for _, parts := range app {
+		for _, p := range r {
+			result = append(result, p+parts)
+		}
+	}
+
+	return result
 }
 
 func solvePart2(depth int, pattern, mask []int, acc *Accumulator) {
